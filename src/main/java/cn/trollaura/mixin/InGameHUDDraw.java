cfff63367d44cb6f62d9761f51d76bfd13fd0655
tRollaURa_ <berus1612@gmail.com>
2026-02-16 12:58:54 +0800

    更新到1.21.1

--- src/main/java/cn/trollaura/mixin/InGameHUDDraw.java
+++ src/main/java/cn/trollaura/mixin/InGameHUDDraw.java
@@ -4,6 +4,7 @@ import cn.trollaura.event.EventBus;
 import cn.trollaura.event.events.DrawEvent;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class InGameHUDDraw {
 
-    @Inject(method = "render",at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;enableBlend()V",ordinal = 3))
-    public void onRender(DrawContext context, float tickDelta, CallbackInfo ci) {
-        EventBus.INSTANCE.post(new DrawEvent(context,tickDelta));
+    @Inject(method = "render",at = @At(value = "HEAD"))
+    public void onRender(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        EventBus.INSTANCE.post(new DrawEvent(context,tickCounter.getTickDelta(true)));
     }
 }
